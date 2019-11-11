f339f3c2faeac2f65968261ed4123c643b042026
Gabriel Souza <gabriel_feg@hotmail.com>
2019-11-11 14:29:02 -0300

    update svm

--- techniques/svm.py
+++ techniques/svm.py
@@ -1,7 +1,10 @@
-import load_dataset as dataset 
+import load_dataset as loader 
+import results
 import tfidf 
 import numpy as np 
 import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
 
 bag_of_words = {}
 total_positive_words = 0
@@ -20,8 +23,9 @@ def word_tokenize(phrase, stop_words):
             new_list.append(word)
     return new_list
 
-def preprocess ():
-    positive, negative, stop_words = dataset.load()
+def preprocess (value="TRAIN"):
+
+    positive, negative, stop_words = loader.load(value)
     
     positive = lower_case(positive)
     negative = lower_case(negative)
@@ -56,52 +60,203 @@ def create_bag_of_words (pos, neg):
             else:
                 bag_of_words[term]['neither'] = bag_of_words[term]['neither'] + 1
         
-    return bag_of_words, total_positive_words, total_negative_words
 
 def score_list_words (list_words):
     global bag_of_words, total_negative_words, total_positive_words
-    print (list_words, len(list_words))
+
     if (len(list_words) < 1): return 0, 0
     offensive_score = 0
     neither_score = 0
 
     for word in list_words:
-        offensive_score += (bag_of_words[word]['offensive']/total_positive_words)
-        neither_score += (bag_of_words[word]['neither']/total_negative_words)
-    
+        if word in bag_of_words.keys():
+            offensive_score += (bag_of_words[word]['offensive']/total_positive_words)
+            neither_score += (bag_of_words[word]['neither']/total_negative_words)
+        else: continue
+
     offensive_score = offensive_score / len(list_words)
     neither_score = neither_score / len(list_words)
 
     return offensive_score, neither_score
 
 def create_dataset (positive, negative):
-    pos_x = []
-    pos_y = []
-    neg_x = []
-    neg_y = []
+    
+    new_pos = []
+    new_neg = [] 
 
     for pos in positive:
         p, n = score_list_words(pos)
-        pos_x.append(p)
-        pos_y.append(n) 
+        new_pos.append([p, n]) 
     
     for neg in negative:
         p, n = score_list_words(neg)
-        neg_x.append(p)
-        neg_y.append(n)
+        new_neg.append([p, n])
 
-    plt.figure(figsize=(8,6))
-    plt.scatter(pos_x,pos_y,marker='+',color='green')
-    plt.scatter(neg_x,neg_y,marker='_',color='red')
-    plt.show()
+    return new_pos, new_neg
 
-def train (dataset, alpha=0.0001, max_epochs=10000):
-    pass
+    # plt.figure(figsize=(8,6))
+    # plt.scatter(pos_x,pos_y,marker='+',color='green')
+    # plt.scatter(neg_x,neg_y,marker='_',color='red')
+    # plt.show()
+
+def train (dataset, classes, max_epochs=500, alpha=0.0001):
+    dataset_size = len(dataset)
+    
+    dataset_train_vector = np.array(dataset)
+    classes_train_vector = np.array(classes)
+
+    # transform classes in a vector
+    classes_train_vector = classes_train_vector.reshape(dataset_size, 1)
+
+    train_positive_feature = dataset_train_vector[:,0]
+    train_negative_feature = dataset_train_vector[:,1]
+
+    # transform features in vectors
+    train_positive_feature = train_positive_feature.reshape(dataset_size, 1)
+    train_negative_feature = train_negative_feature.reshape(dataset_size, 1)
+
+    w_pos = np.zeros((dataset_size, 1))
+    w_neg = np.zeros((dataset_size, 1))
+
+    epochs = 1 
+    while (epochs < max_epochs):
+        value = w_pos * train_positive_feature + w_neg * train_negative_feature
+        production = value * classes_train_vector
+        print("epochs:", epochs)
+
+        count = 0
+        lambda_ = 1.0 / epochs  
+        # adjust weights
+        for val in production:
+            if (val >= 1):
+                cost = 0
+                w_pos = w_pos - alpha * (2 * lambda_ * w_pos)
+                w_neg = w_neg - alpha * (2 * lambda_ * w_neg)
+            else:
+                cost = 1 - val
+                w_pos = w_pos + alpha * (train_positive_feature[count] * classes_train_vector[count] - 2 * lambda_ * w_pos)
+                w_neg = w_neg + alpha * (train_negative_feature[count] * classes_train_vector[count] - 2 * lambda_ * w_neg)
+            count += 1
+        epochs += 1
+
+    return w_pos, w_neg
+
+def compute_metrics(real, predict):
+    size_ = len(real)
+    
+    fp = 0
+    fn = 0
+    tp = 0
+    tn = 0 
+
+    for i in range(size_):
+        if real[i] == -1 and predict[i] == -1:
+            tp += 1
+        elif real[i] == -1 and predict[i] == 1:
+            fn += 1
+        elif real[i] == 1 and predict[i] == 1:
+            tn += 1
+        elif real[i] == 1 and predict[i] == -1:
+            fp += 1
+
+    results.print_results (tp, tn, fp, fn)
+ 
+def test (w_pos, w_neg, size_dataset_train):
+    positive, negative = preprocess("TEST")
+    positive, negative = create_dataset(positive, negative)
+
+    print ("w_pos:", w_pos)
+    print ("w_neg:", w_neg)
+
+    # create vector of results and dataset
+    dataset = []
+    classes = []
+
+    for val in positive:
+        dataset.append(val)
+        classes.append(-1)
+    
+    for val in negative:
+        dataset.append(val)
+        classes.append(1)
+    
+    size_dataset_test = len(dataset)
+
+    dataset_test_vector = np.array(dataset)
+    classes_test_vector = np.array(classes)
+
+    # transform classes in a vector
+    classes_test_vector = classes_test_vector.reshape(size_dataset_test, 1)
+
+    ## Clip the weights 
+    index = list(range(size_dataset_test, size_dataset_train))
+    w_pos = np.delete(w_pos,index)
+    w_neg = np.delete(w_neg,index)
+
+    w_pos = w_pos.reshape(size_dataset_test, 1)
+    w_neg = w_neg.reshape(size_dataset_test, 1)
+
+    ## Extract the test data features 
+    test_positive_feature = dataset_test_vector[:,0]
+    test_negative_feature = dataset_test_vector[:,1]
+
+    test_positive_feature = test_positive_feature.reshape(size_dataset_test, 1)
+    test_negative_feature = test_negative_feature.reshape(size_dataset_test, 1)
+
+    ## predict
+    y_pred = w_pos * test_positive_feature + w_neg * test_negative_feature
+  
+    predictions = []
+    for value in y_pred:
+        if (val[0] > 1):
+            predictions.append(1)
+        else:
+            predictions.append(-1)
+
+    compute_metrics(classes_test_vector, predictions)
+
+def run ():
+    positive, negative = preprocess()       # get dataset
+    create_bag_of_words(positive, negative) # create bag of words
+    positive, negative = create_dataset(positive, negative) # convert all text in a numeric value
+
+    # create vector of results and dataset
+    dataset = []
+    classes = []
+
+    for val in positive:
+        dataset.append(val)
+        classes.append(-1)
+    
+    for val in negative:
+        dataset.append(val)
+        classes.append(1)
+   
+    dataset, classes = shuffle(dataset, classes)
+    w_pos, w_neg = train(dataset, classes, max_epochs=100)
+    test(w_pos, w_neg, len(dataset))
+
 
 if __name__ == '__main__':
-    positive, negative = preprocess()
-    bag_of_words, total_positive, total_negative = create_bag_of_words(positive, negative)
-    create_dataset(positive, negative)
+    
+    positive, negative = preprocess()       # get dataset
+    create_bag_of_words(positive, negative) # create bag of words
+    positive, negative = create_dataset(positive, negative) # convert all text in a numeric value
 
+    # create vector of results and dataset
+    dataset = []
+    classes = []
 
+    for val in positive:
+        dataset.append(val)
+        classes.append(-1)
+    
+    for val in negative:
+        dataset.append(val)
+        classes.append(1)
+   
+    dataset, classes = shuffle(dataset, classes)
+    w_pos, w_neg = train(dataset, classes, max_epochs=500)
+    test(w_pos, w_neg, len(dataset))
 
+    
